8e3f86987b3ea6571fbb0a1453df19d98ec8ea0a
Pavel_Sakharchuk <sakharchuk.pavel@gmail.com>
2023-11-03 16:00:31 +0100

    6

--- vividus-to-jira-exporter/src/main/java/org/vividus/plugin/jira/VividusToJiraExporterApplication.java
+++ vividus-to-jira-exporter/src/main/java/org/vividus/plugin/jira/VividusToJiraExporterApplication.java
@@ -37,6 +37,7 @@ public class VividusToJiraExporterApplication
 {
     public static void main(String[] args) throws IOException
     {
+        System.setProperty("spring.config.name", "over");
         ApplicationContext context = SpringApplication.run(VividusToJiraExporterApplication.class, args);
         context.getBean(JiraExporter.class).exportResults();
     }
